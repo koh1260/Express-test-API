d86ae16d1888424bbc2663a6d47453805e3e8a43
HaesungKang <koh1260@naver.com>
2023-02-08 22:06:22 +0900

    Fix: session 연결 옵션 변경

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const MySQLStore = require("express-mysql-session")(session);
 require("dotenv").config();
 
 var options = {
-  host: '127.0.0.1',
-  port: 3306,
-  user: "hs",
-  password: "root",
-  database: "instagram",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 var sessionStore = new MySQLStore(options);
