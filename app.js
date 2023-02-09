7c9f72366f996e109bc34af20a1f06b86409d826
HaesungKang <koh1260@naver.com>
2023-02-09 16:39:56 +0900

    Feat: Logout 기능 구현

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const db = require("./db");
 const auth = require('./router/auth');
+const cors = require("cors");
 require("dotenv").config();
 
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  credential: true
+}
 
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
--- router/auth.js
+++ router/auth.js
@@ -1,74 +1,93 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const db = require("../db");
-const session = require('express-session');
+const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
 
 var options = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  };
-  
-  var sessionStore = new MySQLStore(options);
-  // use
-  router.use(session({
-      key: 'session_cookie_name',
-      secret: process.env.SESSION_SECRET,
-      store: sessionStore,
-      resave: false,
-      saveUninitialized: false
-  }));
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+var sessionStore = new MySQLStore(options);
+// use
+router.use(
+  session({
+    key: "session_cookie_name",
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // user 조회
 router.get("/user/:user_id", (req, res) => {
-    const userId = req.params.user_id;
-    db.query("SELECT * FROM user WHERE user_id = ?", userId, (err, row) => {
-      console.log(row);
-      if (err) console.log(err);
-      if (row.length === 0) return res.status(400).send("존재하지 않는 user");
-      return res.json(row[0]);
-    });
+  const userId = req.params.user_id;
+  db.query("SELECT * FROM user WHERE user_id = ?", userId, (err, row) => {
+    console.log(row);
+    if (err) console.log(err);
+    if (row.length === 0) return res.status(400).send("존재하지 않는 user");
+    return res.json(row[0]);
   });
+});
 
-  // user 테이블에 데이터 삽입
+// user 테이블에 데이터 삽입
 router.post("/register", (req, res) => {
-    const body = req.body
-    const params = [
-      body.email,
-      body.name,
-      body.nickname,
-      bcrypt.hashSync(body.password),
-    ];
-  
-    db.query(
-      "INSERT INTO user(email,  name, nickname, password) VALUE(?,?,?,?)",
-      params,
-      (err, row) => {
-        if (err) console.log(err);
-        res.sendStatus(400).send("Bad Request");
-      }
-    );
-    res.sendStatus(200).send("Success");
-  });
+  const body = req.body;
+  const params = [
+    body.email,
+    body.name,
+    body.nickname,
+    bcrypt.hashSync(body.password),
+  ];
 
-  // 로그인, 세션 생성
-router.post("/login", (req, res) => {
-    const body = req.body;
-    const email = body.email;
-    const password = body.password;
-  
-    db.query("SELECT * FROM user WHERE email = ?", email, (err, row) => {
+  db.query(
+    "INSERT INTO user(email,  name, nickname, password) VALUE(?,?,?,?)",
+    params,
+    (err, row) => {
       if (err) console.log(err);
-      if (row.length === 0) return res.status(400).send("아이디 없음");
-      if (row[0].password !== password)
-        return res.status(400).send("비밀번호 불일치");
-      req.session.is_logined = true;
-      req.session.nickname = row[0].nickname;
-      return res.status(200).send(req.session);
-    });
-  });
+      res.sendStatus(400).send("Bad Request");
+    }
+  );
+  res.sendStatus(200).send("Success");
+});
 
-  module.exports = router;
+// 로그인, 세션 생성
+// 데이터 없음 400, 아이디 없음 404, 비밀번호 불일치 401
+router.post("/login", (req, res) => {
+  const body = req.body;
+  const email = body.email;
+  const password = body.password;
+
+  if (Object.keys(body).length === 0) return res.status(400).send("no data");
+
+  db.query("SELECT * FROM user WHERE email = ?", email, (err, row) => {
+    if (err) console.log(err);
+    if (row.length === 0) return res.status(404).send("존재하지 않는 계정");
+    if (row[0].password !== password)
+      return res.status(401).send("비밀번호 불일치");
+    req.session.is_logined = true;
+    req.session.nickname = row[0].nickname;
+    return res.status(200).send(req.session);
+  });
+});
+
+router.get("/logout", (req, res) => {
+  const session = req.session;
+
+  if (session.is_logined) {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return res.status(200).send("logout success");
+      }
+    });
+  }
+});
+
+module.exports = router;
